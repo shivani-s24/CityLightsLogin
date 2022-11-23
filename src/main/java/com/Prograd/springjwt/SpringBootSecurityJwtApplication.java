63439117b1b69ced06e6ffc561077e8c0b87d12d
Shivani Sathyanarayan <Shivani.S@ivycomptech.com>
2022-11-23 11:10:46 +0530

    Backend

---
+++ src/main/java/com/Prograd/springjwt/SpringBootSecurityJwtApplication.java
@@ -0,0 +1,21 @@
+package com.Prograd.springjwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class SpringBootSecurityJwtApplication implements CommandLineRunner {
+@Autowired
+private PasswordEncoder passwordEncoder;
+	public static void main(String[] args) {
+    SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(this.passwordEncoder.encode("Shivani@123"));
+	}
+}
